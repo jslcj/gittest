420fd1a1b6ef461a8a6b6c2ed96810ee638d6cfc
jslcj <18482030097@163.com>
2018-06-02 00:04:53 +0800

    添加toString方法

--- src/main/java/com/js/Demo.java
+++ src/main/java/com/js/Demo.java
@@ -4,4 +4,10 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("pppppppppppppppppppppppp");
     }
+
+    @Override
+    public String toString() {
+        System.out.println("ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp");
+        return "1111111111";
+    }
 }
